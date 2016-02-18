a87acbbd3c14ee330ea98a99607f95160e8e1d54
magicland12 <sw_02b@mail.ru>
2016-02-18 20:44:46 +0200

    screenshot chapturing

--- MyTest/src/captureScreenshot/FacebookScreenshot.java
+++ MyTest/src/captureScreenshot/FacebookScreenshot.java
@@ -1,5 +1,13 @@
 package captureScreenshot;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class FacebookScreenshot {
+	
+	public void captureScreenshot(){
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+	}
 
 }
